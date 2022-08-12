b30ac844d68fefc7be2c7c0adacc47b9cc68cbdb
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2022-08-12 08:00:11 +0800

    Update xray_installation_grpc_ws.sh
    优化续签问题

--- xray_installation_grpc_ws.sh
+++ xray_installation_grpc_ws.sh
@@ -56,8 +56,8 @@ chown www-data.www-data $ssl_dir/xray.*
 ## 把申请证书命令添加到计划任务
 echo -n '#!/bin/bash
 /etc/init.d/nginx stop
-"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /root/renew_ssl.log
-/etc/init.d/nginx start
+wait;"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /root/renew_ssl.log
+wait;/etc/init.d/nginx start
 ' > /usr/local/bin/ssl_renew.sh
 chmod +x /usr/local/bin/ssl_renew.sh
 (crontab -l;echo "15 03 * * * /usr/local/bin/ssl_renew.sh") | crontab
